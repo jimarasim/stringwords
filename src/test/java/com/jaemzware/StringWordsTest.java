0aa4d1725172dbfe06a109f8a3a6963f5451ef53
JAMES K ARASIM <jaemzware@hotmail.com>
2019-08-17 22:35:49 -0700

    Expanded some tests

--- src/test/java/com/jaemzware/StringWordsTest.java
+++ src/test/java/com/jaemzware/StringWordsTest.java
@@ -2,43 +2,81 @@ package com.jaemzware;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
 public class StringWordsTest {
 
     @Test
     public void validWordOneCombination(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("it").size()==1);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("it");
+
+        assertTrue(words.size()==1);
+        assertTrue( words.get(0) == "it");
     }
 
     @Test
     public void validWordMultipleCombinations(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("cat").size()>0);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("cat");
+
+        assertTrue(words.size()==4);
+        assertTrue(words.contains("cat"));
+        assertTrue(words.contains("act"));
+        assertTrue(words.contains("a"));
+        assertTrue(words.contains("at"));
     }
 
     @Test
     public void invalidWordOneCombination(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("ti").size()==1);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("ti");
+
+        assertTrue(words.size()==1);
+        assertTrue(words.get(0)=="it");
     }
 
     @Test
     public void invalidWordMultipleCombinations(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("atc").size()>0);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("atc");
+
+        assertTrue(words.size()==4);
+        assertTrue(words.contains("cat"));
+        assertTrue(words.contains("act"));
+        assertTrue(words.contains("a"));
+        assertTrue(words.contains("at"));
     }
 
     @Test
     public void invalidWordNoCombinations(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("zzzzzzz").size()==0);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("zzzzzzz");
+
+        assertTrue(words.size()==0);
     }
 
     @Test
     public void emptyString(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("").size()==0);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("");
+
+        assertTrue(words.size()==0);
     }
 
     @Test
     public void wordWithSpaces(){
-        assertTrue(StringWords.getAllCombinationsOfEnglishWords("ad hoc").size()>0);
+
+        List<String> words = StringWords.getAllCombinationsOfEnglishWords("ad hoc");
+
+        assertTrue(words.size()==0);
+        assertTrue(words.contains("ad hoc"));
+        assertTrue(words.contains("cad"));
+        assertTrue(words.contains("a"));
+        assertTrue(words.contains("oh"));
+
     }
 
 
